a322f32094fe2b77bfbf561bd95793f2cdec1487
Shaun Zacharia <shaunzacharia5@gmail.com>
2020-05-21 12:45:49 +0530

    Update CNN_train.py
    Added comments and docstring

--- CNN_train.py
+++ CNN_train.py
@@ -19,6 +19,19 @@ import keras.backend as K
 K.set_image_data_format('channels_last')
 
 def initializeData():
+    """
+    Returns
+    -------
+    X_train : ndarray
+        Train input, shape = (no. of train examples, output_shape, output_shape, 1).
+    y_train : ndarray
+        Train output, shape = (no. of train examples, no. of blocks).
+    X_test : ndarray
+        Test input, shape = (no. of test examples, output_shape, output_shape, 1).
+    y_test : ndarray
+        Test output, shape = (no. of test examples, no. of blocks).
+    """
+    # reading and reshaping the data for desired form of keras model
     X_train = cv2.imread("data/delay (1).jpg", 0)
     X_train = X_train.reshape((1, 120, 120, 1))
     parts = ["delay", "gain", "gre", "ramp", "sine", "stop"]
@@ -42,12 +55,14 @@ def initializeData():
             im = im.reshape((1, 120, 120, 1))
             X_test = np.concatenate((X_test, im), axis = 0)
     
+    # normaizing data (this is sufficient for image data)
     X_train = X_train/255.
     X_test = X_test/255.
     
     y_train = sorted(parts*15)
     y_test = sorted(parts*4)
     
+    # one-hot encoding the y data
     y_train = pd.get_dummies(pd.Series(y_train).astype('category')).to_numpy()
     y_test = pd.get_dummies(pd.Series(y_test).astype('category')).to_numpy()
     
